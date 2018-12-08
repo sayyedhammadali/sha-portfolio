8fcf6bbe99ba91179cffef31fc041e87c9a91c35
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-12-08 19:27:20 +0100

    Updates

---
+++ precache-manifest.e024758ff0c36533deac2e7723203358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "451120df8a0bb021c65d",
+    "url": "/sha-portfolio/static/js/runtime~main.451120df.js"
+  },
+  {
+    "revision": "119112ca0051cad589c4",
+    "url": "/sha-portfolio/static/js/main.119112ca.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/sha-portfolio/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "119112ca0051cad589c4",
+    "url": "/sha-portfolio/static/css/main.7fff8a3b.chunk.css"
+  },
+  {
+    "revision": "9bc8a2548e9a271e45f3003224ce45fe",
+    "url": "/sha-portfolio/index.html"
+  }
+];
