a30ccbc747ca5d736b793e2317da7b293719d362
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-12-09 23:00:23 +0100

    Updates

---
+++ precache-manifest.7aa9f1bc9a5c34cdf21d0ee215d4ae51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3fa5a5f9ad2c1bfe567",
+    "url": "/static/js/main.d3fa5a5f.chunk.js"
+  },
+  {
+    "revision": "cc26687f23aacf42e9ad",
+    "url": "/static/js/1.cc26687f.chunk.js"
+  },
+  {
+    "revision": "d3fa5a5f9ad2c1bfe567",
+    "url": "/static/css/main.0ad04e0e.chunk.css"
+  },
+  {
+    "revision": "45c8a1e8f539097e59cf49517027b49b",
+    "url": "/index.html"
+  }
+];
