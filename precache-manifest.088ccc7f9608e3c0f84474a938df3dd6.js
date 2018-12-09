197bd86a5909a0edac8aa3f6f7d0c88c79a03887
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-12-09 19:35:51 +0100

    Updates

---
+++ precache-manifest.088ccc7f9608e3c0f84474a938df3dd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "451120df8a0bb021c65d",
+    "url": "/sha-portfolio/static/js/runtime~main.451120df.js"
+  },
+  {
+    "revision": "3cefbf397e7963ddbc69",
+    "url": "/sha-portfolio/static/js/main.3cefbf39.chunk.js"
+  },
+  {
+    "revision": "cc26687f23aacf42e9ad",
+    "url": "/sha-portfolio/static/js/1.cc26687f.chunk.js"
+  },
+  {
+    "revision": "3cefbf397e7963ddbc69",
+    "url": "/sha-portfolio/static/css/main.0ad04e0e.chunk.css"
+  },
+  {
+    "revision": "3535915428963980eb663656949b70f5",
+    "url": "/sha-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sha-portfolio/precache-manifest.8d4aed64dc521dbc37769b9a54817444.js"
+  "/sha-portfolio/precache-manifest.088ccc7f9608e3c0f84474a938df3dd6.js"
 );
 
 workbox.clientsClaim();
