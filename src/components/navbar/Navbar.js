d3be26d1048827524273c62ece0de4d3977ce4b4
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-12-18 13:51:10 +0100

    style fixes navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,30 +10,32 @@ const Navbar = () => {
 
   return (
     <div id="navbar">
-      <nav className="navbar navbar-expand-lg bg-dark">
-        <a className="navbar-brand" onClick={scroll} data-id="landing">sHa</a>
-        <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-          <i className="fas fa-chevron-down" style={{color: 'white'}}></i>
-          <i className="fas fa-chevron-up"  style={{color: 'white'}}></i>
-        </button>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg bg-gradient-primary text-white">
+          <a className="navbar-brand" onClick={scroll} data-id="landing">sHa</a>
+          <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <i className="fas fa-chevron-down" style={{color: 'white'}}></i>
+            <i className="fas fa-chevron-up"  style={{color: 'white'}}></i>
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
-              <a className="nav-link"  onClick={scroll} data-id="landing">home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link"  onClick={scroll} data-id="about">about</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link"  onClick={scroll} data-id="portfolio">portfolio</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" onClick={scroll} data-id="contact">contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+              <li className="nav-item active">
+                <a className="nav-link text-uppercase"  onClick={scroll} data-id="landing">home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-uppercase"  onClick={scroll} data-id="about">about</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-uppercase"  onClick={scroll} data-id="portfolio">portfolio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-uppercase" onClick={scroll} data-id="contact">contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
